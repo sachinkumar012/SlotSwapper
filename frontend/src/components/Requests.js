2211fd933659545b1c645d0ee3502faf259c525d
sachinkumar012 <sachinyadav887780@gmail.com>
2025-11-05 23:35:16 +0530

    jj

--- frontend/src/components/Requests.js
+++ frontend/src/components/Requests.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useAuth } from "../context/AuthContext";
-import "./requests.css";
+import "./Requests.css";
 
 function Request() {
   const [incoming, setIncoming] = useState([]);
